f0e4596bf56c457b0aebae1be150bcb4b1a6f0fc
udaykumarkp10 <159992305+udaykumarkp10@users.noreply.github.com>
2024-05-14 16:59:33 +0530

    Update new_new.py

--- new_new.py
+++ new_new.py
@@ -5,19 +5,27 @@ import ctypes
 # Define byte values
 one = ctypes.c_uint8(0x03).value
 two = ctypes.c_uint8(0x00).value
-three = ctypes.c_uint8(0x64).value
+three = ctypes.c_uint8(0x50).value
 four = ctypes.c_uint8(0xFF).value
 five = ctypes.c_uint8(0xFF).value
 six = ctypes.c_uint8(0xFF).value
 seven = ctypes.c_uint8(0xFF).value
 
-# Initialize SPI
-spi = spidev.SpiDev()
-spi.open(2, 0)  # Open SPI bus 2, device 0
-spi.max_speed_hz = 30000000  # Set SPI speed to 30 MHz
-spi.mode = 0b11
 
-counter = 0
+# SPI setup
+spi = spidev.SpiDev()
+spi.open(2, 0)  # Assuming SPI bus 0, device 0
+spi.max_speed_hz = 30000000  # Adjust as needed
+spi.mode = 3
+
+# Additional settings
+# spi.no_cs = True            # Disable hardware CS
+spi.bits_per_word = 8  # Data size is 8 bits (SPI_DATASIZE_8BIT)
+spi.cshigh = False  # Use hardware CS
+spi.lsbfirst = False  # MSB first (SPI_FIRSTBIT_MSB)
+spi.threewire = False  # Use 3-wire mode (SPI_DIRECTION_2LINES)
+# spi.loop = False  # Disable loopback mode
+
 while True:
   tx_data = [one, two, three, four, five, six, seven]
   # Perform SPI transaction
