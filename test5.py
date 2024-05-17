3fb50a384d35734d028a54b478712c94536ba914
udaykumarkp10 <159992305+udaykumarkp10@users.noreply.github.com>
2024-05-17 14:51:06 +0530

    Delete test5.py

--- test5.py
+++
@@ -1,8 +0,0 @@
-import sys
-
-if sys.byteorder == 'little':
-    print("Your system is using little-endian byte order.")
-elif sys.byteorder == 'big':
-    print("Your system is using big-endian byte order.")
-else:
-    print("Unable to determine byte order.")
