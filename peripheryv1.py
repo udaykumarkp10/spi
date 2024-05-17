d57513677df5cd43dd5a0b167541a23b9700aeec
udaykumarkp10 <159992305+udaykumarkp10@users.noreply.github.com>
2024-05-17 11:59:28 +0530

    Update peripheryv1.py

--- peripheryv1.py
+++ peripheryv1.py
@@ -1,3 +1,4 @@
+import time
 from periphery import SPI
 
 # Open SPI device
@@ -6,11 +7,20 @@ spi = SPI("/dev/spidev2.0", 0, 1000000)  # SPI bus 2, device 0, 1 MHz
 # Data to send (example data, modify as per your device's protocol)
 tx_data = [0x01, 0x02, 0x03, 0x04]
 
-# Send and receive data
-rx_data = spi.transfer(tx_data)
+try:
+    while True:
+        # Send and receive data
+        rx_data = spi.transfer(tx_data)
 
-# Print received data
-print("Received data:", rx_data)
+        # Print received data
+        print("Received data:", rx_data)
 
-# Close SPI device
-spi.close()
+        # Delay for 0.1 seconds
+        time.sleep(0.1)
+
+except KeyboardInterrupt:
+    print("Ctrl+C detected. Exiting gracefully.")
+
+finally:
+    # Close SPI device
+    spi.close()
