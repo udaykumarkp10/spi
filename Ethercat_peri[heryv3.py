b8ba3ef03818ae9e1fd3c50812b20f94195bf62d
udaykumarkp10 <159992305+udaykumarkp10@users.noreply.github.com>
2024-05-23 16:22:52 +0530

    Update Ethercat_peri[heryv3.py

--- Ethercat_peri[heryv3.py
+++ Ethercat_peri[heryv3.py
@@ -28,6 +28,9 @@ class ULONG(Union):
     _fields_ = [("LANLong", c_uint32),    # uint32_t LANLong
                 ("LANByte", c_uint8 * 4)] # uint8_t LANByte[4]
 
+
+"""
+ 
 def Etc_Read_Reg(request, address, length):
     Result = ULONG()  # Initialize Result as ULONG instance
     Addr = c_uint16(address)  # Initialize Addr as c_uint16 and set address
@@ -58,6 +61,54 @@ def Etc_Read_Reg(request, address, length):
 
     print(Result.LANLong)
     return Result.LANLong
+
+"""
+
+
+def Etc_Read_Reg(address, length):
+    Result = ULONG()  # Initialize Result as ULONG instance
+    Addr = UWORD()    # Initialize Addr as UWORD instance and set address
+    xfrbuf = (ctypes.c_uint8 * 7)()  # Create buffer for SPI transfer
+
+    Addr.LANWord = address
+
+    xfrbuf[0] = COMM_SPI_READ  # SPI read command
+    xfrbuf[1] = Addr.LANByte[1]  # Address high byte
+    xfrbuf[2] = Addr.LANByte[0]  # Address low byte
+
+    print()
+    print("Entered Read_reg")
+    print()
+    print("xfrbuf before filling: ", end="")
+    for i in range(3):
+        print("{:02X} ".format(xfrbuf[i]), end="")          
+
+    for i in range(length):
+        xfrbuf[i + 3] = DUMMY_BYTE  # Fill dummy bytes after address bytes
+
+    print("\nxfrbuf after filling: ", end="")
+    for i in range(7):
+        print("{:02X} ".format(xfrbuf[i]), end="")
+
+    request.set_value(LINE, Value.INACTIVE)
+    response = spi.transfer(xfrbuf_list)
+    request.set_value(LINE, Value.ACTIVE)
+
+
+    # print("\nresponse:", response)  # Print the response received from spi.xfer(xfrbuf)
+
+    print()
+    print("Response in hexadecimal:")
+    for byte in response:
+        print("{:02X}".format(byte), end=" ")
+
+    # response = spi.xfer2(list(xfrbuf))  # Perform SPI transfer and receive response
+    Result.LANLong = 0
+
+    for i in range(length):
+        Result.LANByte[i] = response[i + 3]  # Assign received bytes to Result.LANByte[i]
+    return Result.LANLong
+
 
 def etc_init():
     # Initialize EtherCAT interface
