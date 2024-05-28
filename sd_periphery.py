19590b3de6bf485437550ca49a2ea84fcad2fd07
udaykumarkp10 <159992305+udaykumarkp10@users.noreply.github.com>
2024-05-28 11:51:17 +0530

    Update sd_periphery.py

--- sd_periphery.py
+++ sd_periphery.py
@@ -4,20 +4,25 @@ from periphery import SPI
 # Initialize SPI interface with mode and max_speed
 spi = SPI("/dev/spidev2.0", mode=0, max_speed=500000)
 
+# Create mount point if it doesn't exist
+mount_point = "/mnt/sdcard"
+if not os.path.exists(mount_point):
+    os.makedirs(mount_point)
+
 # Mount SD card filesystem
-os.system("mount /dev/mmcblk0p1 /mnt/sdcard")
+os.system("mount /dev/mmcblk0p1 " + mount_point)
 
 # Write data to a file
-with open("/mnt/sdcard/test.txt", "w") as f:
+with open(os.path.join(mount_point, "test.txt"), "w") as f:
     f.write("Hello, SD card!")
 
 # Read data from the file
-with open("/mnt/sdcard/test.txt", "r") as f:
+with open(os.path.join(mount_point, "test.txt"), "r") as f:
     data = f.read()
     print("Data read from file:", data)
 
 # Unmount SD card filesystem
-os.system("umount /mnt/sdcard")
+os.system("umount " + mount_point)
 
 # Close SPI interface
 spi.close()
