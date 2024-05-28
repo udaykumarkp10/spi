bdd5a29febc5edc0437a938883e1644778229989
udaykumarkp10 <159992305+udaykumarkp10@users.noreply.github.com>
2024-05-28 11:23:19 +0530

    Update sd_periphery.py

--- sd_periphery.py
+++ sd_periphery.py
@@ -4,24 +4,22 @@ import time
 # Initialize SPI communication
 spi = periphery.SPI("/dev/spidev2.0", mode=0, max_speed=500000)
 
-# Function to send data over SPI
-def spi_send(data):
-    spi.transfer(data)
-
-# Function to receive data over SPI
-def spi_receive(length):
-    return spi.transfer([0x00] * length)
+# Function to perform full-duplex SPI communication
+def spi_full_duplex(data_out):
+    # Perform full-duplex SPI communication
+    data_in = spi.transfer(data_out)
+    return data_in
 
 # Example usage
 def main():
     # Example data to send
     data_to_send = [0xAA, 0xBB, 0xCC, 0xDD]
 
-    # Send data
-    spi_send(data_to_send)
+    # Perform full-duplex SPI communication
+    received_data = spi_full_duplex(data_to_send)
 
-    # Receive data (assuming we want to receive the same number of bytes as sent)
-    received_data = spi_receive(len(data_to_send))
+    # Print sent and received data
+    print("Sent data:    ", data_to_send)
     print("Received data:", received_data)
 
 if __name__ == "__main__":
